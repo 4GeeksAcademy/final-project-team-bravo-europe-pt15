cb22ccd242df53ce3cbad74a517187355978e4ac
Admir Skomorac <admir.skomorac@outlook.com>
2024-05-07 08:38:42 +0000

    added endpoint for login and signup

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,13 +5,100 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route("/login", methods=['POST'])
+def create_login_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
+    # Query your database for username and password
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # Create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id }), 200
+
+
+@api.route('/signup', methods=['POST'])
+def register_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if email is None:
+        return jsonify({"msg": "Email can't be empty"}), 400
+
+    if password is None:
+        return jsonify({"msg": "Password can't be empty"}), 400
+
+    # Check if user with the same email already exists
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "User with this email already exists"}), 400
+
+    # If user doesn't exist, create a new user
+    user = User(
+        email=email,
+        password=password,
+        is_active=True
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"msg": "New User created"}), 201
+
+
+@api.route('/user/<int:user_id>', methods=['DELETE'])
+@jwt_required()  # Ensure that only authenticated users can delete a user
+def delete_user(user_id):
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+
+    # Check if the current user is trying to delete their own account
+    if current_user_id != user_id:
+        return jsonify({"msg": "You are not authorized to delete this user"}), 403
+
+    # Query the user to be deleted
+    user_to_delete = User.query.get(user_id)
+
+    # Check if the user exists
+    if user_to_delete is None:
+        return jsonify({"msg": "User not found"}), 404
+
+    # Delete the user
+    db.session.delete(user_to_delete)
+    db.session.commit()
+
+    return jsonify({"msg": "User deleted successfully"}), 200
+
+
+
+
+
+# Protect a route with jwt_required, which will kick out requests without a valid JWT
+@api.route("/validate", methods=["GET"])
+@jwt_required()
+def validate():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
+
+
+
+# Test endpoint
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
--- src/app.py
+++ src/app.py
@@ -6,10 +6,12 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 
 # from models import Person
 
@@ -18,6 +20,10 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.getenv("my_key")   # This should be unique
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -66,7 +72,6 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
-
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
