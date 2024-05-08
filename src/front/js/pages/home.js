b232bd388f5ab28327dd247adfbf656c27dc0717
Admir Skomorac <admir.skomorac@outlook.com>
2024-05-08 19:24:20 +0000

    no comment

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,12 +1,9 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import React from "react";
 import "../../styles/home.css";
 import UploadWidget from "../component/UploadWidget";
 import Login from "./login";
 
 export const Home = () => {
-  const { store, actions } = useContext(Context);
-
   return (
     <div className="text-center mt-5">
       <h1>Image Upload</h1>
