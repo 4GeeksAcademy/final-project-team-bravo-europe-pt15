ff5b6a200ed6f9740f89c9f10d6af9ef5bd3f787
Admir Skomorac <admir.skomorac@outlook.com>
2024-05-09 10:52:14 +0000

    change button in Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,13 +6,11 @@ export const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
       <div className="container">
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+          <span className="navbar-brand mb-0 h1">MAFL Home</span>
         </Link>
         <div className="ml-auto">
           <Link to="/login-test">
-            <button className="btn btn-primary">
-              Login
-            </button>
+            <button className="btn btn-primary">Login</button>
           </Link>
         </div>
       </div>
